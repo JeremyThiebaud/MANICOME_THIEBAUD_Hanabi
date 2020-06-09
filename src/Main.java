e145f624b12ffd655b2764b96b65fea828222300
smanicome <s.manicome@gmail.com>
2020-06-09 18:19:28 +0200

    Super modification of View

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import fr.umlv.hanabi.controller.GameController;
+import fr.umlv.hanabi.view.View;
 import fr.umlv.hanabi.view.ZenView;
 
 public class Main {
     public static void main(String[] args) {
-        GameController g = new GameController(new ZenView());
+        View view = new ZenView();
+        GameController g = new GameController(view);
         g.menu();
     }
 }
